89ee77300de3e4a873940339fed904380d9e9f52
Leongr14 <leonguerrare14@gmail.com>
2026-02-06 13:44:13 +0100

    PACMAAAAAANNN

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,28 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public int score = 0;
+    public int maxScore = 24;
     public TextMeshProUGUI scoreText;
 
+    public GameObject gameOverUI;
+    public GameObject victoryUI;
+
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        gameOverUI.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     private void Awake()
     {
         if (Instance == null) 
@@ -24,7 +39,17 @@ public class GameManager : MonoBehaviour
         score++;
         UpdateScoreUI();
         Debug.Log("Puntos: " + score);
-        
+        if (score >= maxScore)
+        {
+            Victory();
+        }
+
+
+    }
+    public void Victory()
+    {
+        Time.timeScale = 0f;
+        victoryUI.SetActive(true);
     }
     private void UpdateScoreUI()
     {
